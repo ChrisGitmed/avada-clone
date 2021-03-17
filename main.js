1bf15cd26a3ff4029a5c0a66801489b7a8085f69
Chris Gitmed <cgitmed@gmail.com>
2021-03-17 13:21:05 -0700

    Added CSS transitions to slide the menu in and out of view.

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ const $investorsIcon = document.querySelector('#investors-icon');
 const $investorsMenu = document.querySelector('.investors-menu');
 
 $menuIcon.addEventListener('click', () => {
-    $navList.classList.toggle('hidden');
+    $navList.classList.toggle('active')
 });
 
 $aboutIcon.addEventListener('click', () => {
-    $aboutMenu.classList.toggle('hidden');
+    $aboutMenu.classList.toggle('active');
 })
 
 $investorsIcon.addEventListener('click', () => {
-    $investorsMenu.classList.toggle('hidden');
+    $investorsMenu.classList.toggle('active');
 })
