955ebdba6131b6dc9727544839c21b04da4c62e5
Chris Gitmed <cgitmed@gmail.com>
2021-03-16 20:22:59 -0700

    Add an event listener on the about icon to toggle the submenu

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const $menuIcon = document.querySelector('.lnr-menu');
 const $navList = document.querySelector('header > ul');
+const $aboutIcon = document.querySelector('#about-icon');
+const $aboutMenu = document.querySelector('.about-menu');
 
 $menuIcon.addEventListener('click', () => {
     $navList.classList.toggle('hidden');
 });
+
+$aboutIcon.addEventListener('click', () => {
+    $aboutMenu.classList.toggle('hidden');
+})
